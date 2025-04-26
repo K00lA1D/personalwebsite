cc59945f97b83a4abf33b5f45606ced8711826bc
Sid Nair <sidnair2003@gmail.com>
2025-04-26 15:00:57 -0700

    Moving things outside of project folder

--- old/src/GenericSVG.js
+++
@@ -1,9 +0,0 @@
-import './style/SVG.css'
-
-const GenericSVG = ({ src, color }) => (
-    <div className="SVG-Container">
-      <img src={src} alt="SVG Background" className="SVG-Image" style={{ filter: `hue-rotate(${color}deg)` }} />
-    </div>
-  );
-
-export default GenericSVG;
